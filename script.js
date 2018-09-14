45ae96c43b070c956984f81e6dc084de127f973d
Pooja patel <poojavpatel71@gmail.com>
2018-09-14 21:48:31 +0530

    function for finding cities done

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 /*jshint esversion: 6 */
 const endpoint="https://raw.githubusercontent.com/nshntarora/Indian-Cities-JSON/master/cities.json";
 
-const cities=[];
+const allCities=[];
 
 /* Fetch returns a promise 
    The json() method takes a Response stream and returns a promise that resolves with the result of parsing the body text as JSON.
    the pro.json() method again returns a promise
+   if we directly push to cities it will be an array of array,hence we use ... spread operator
 */
 fetch(endpoint)
     .then(rawdata => rawdata.json())
     .then(data => {
         console.log('data:', data);
-        cities.push(data);
-        console.log('cities', cities);
+        allCities.push(...data);
+        console.log('allCities', allCities);
     })
     .catch(err => console.log("Error occured :"+err));
 
+/* 'gi' means globally and case Insensative 
+   Test with findCities("mo",allCities); to list all cities that contain mo
+*/
+function findCities(whatToFind,allCities) {
+    return allCities.filter(place => {
+        const regex = new RegExp(whatToFind, 'gi');
+        return place.name.match(regex);
+    });
+}
